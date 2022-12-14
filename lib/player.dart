8e4c5be80f2ffe25fc2bfd049ab6b7f0fb59e9d2
Yannick Mauray <yannick.mauray@arcantel.ch>
2022-12-14 11:11:32 +0100

    feat: limit view distance and fade rays

--- lib/player.dart
+++ lib/player.dart
@@ -3,7 +3,6 @@ import 'package:flame/extensions.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/rendering.dart';
 import 'package:rpgmap/game.dart';
-import 'package:rpgmap/wall.dart';
 
 class Player extends PositionComponent with HasGameRef<MyGame>, Draggable {
   Player(Color color)
@@ -13,79 +12,87 @@ class Player extends PositionComponent with HasGameRef<MyGame>, Draggable {
         super(size: Vector2.all(64), anchor: Anchor.center);
 
   final overlayPainter = Paint()..color = const Color(0xff666666);
-  final rayPainter = Paint()
-    ..color = const Color(0xFFFFFFFF)
-    ..strokeWidth = 2;
   final blackFilledPainter = Paint()
     ..color = const Color(0xFF000000)
     ..style = PaintingStyle.fill;
 
+  static const kViewMax = 500.0;
+
   Paint playerPainter;
+  late Offset c;
+  late Shader rayShader;
+  late Paint rayPainter;
+
+  @override
+  Future<void>? onLoad() {
+    c = (size / 2).toOffset();
+    rayShader =
+        const RadialGradient(colors: [Color(0xFFFFFFFF), Color(0x00FFFFFF)])
+            .createShader(
+      Rect.fromCircle(
+        center: c,
+        radius: kViewMax,
+      ),
+    );
+    rayPainter = Paint()..shader = rayShader;
+
+    return super.onLoad();
+  }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    final c = (size / 2).toOffset();
-    final ray = Vector2(-1, 1).normalized();
-    const nrays = 3000;
-    const step = 6.283185307 / nrays;
+
     final rays = <Offset>[];
+    if (game.activePlayer == this) {
+      final ray = Vector2(-1, 1).normalized();
+      const nrays = 3000;
+      const step = 6.283185307 / nrays;
 
-    for (var n = 0; n < nrays; n++) {
-      final xp = position.x;
-      final yp = position.y;
-      final dx = ray.x;
-      final dy = ray.y;
+      for (var n = 0; n < nrays; n++) {
+        final xp = position.x;
+        final yp = position.y;
+        final dx = ray.x;
+        final dy = ray.y;
 
-      var xmin = double.infinity;
-      var ymin = double.infinity;
-      var dmin = double.infinity;
+        var xmin = double.infinity;
+        var ymin = double.infinity;
+        var dmin = double.infinity;
 
-      for (final wall in [
-        ...game.walls,
-        Wall()
-          ..start = Vector2.zero()
-          ..end = Vector2(game.imageComponent.size.x, 0),
-        Wall()
-          ..start = Vector2(game.imageComponent.size.x, 0)
-          ..end =
-              Vector2(game.imageComponent.size.x, game.imageComponent.size.y),
-        Wall()
-          ..start =
-              Vector2(game.imageComponent.size.x, game.imageComponent.size.y)
-          ..end = Vector2(0, game.imageComponent.size.y),
-        Wall()
-          ..start = Vector2(0, game.imageComponent.size.y)
-          ..end = Vector2.zero(),
-      ]) {
-        final x1 = wall.start.x;
-        final y1 = wall.start.y;
-        final x2 = wall.end.x;
-        final y2 = wall.end.y;
+        for (final wall in game.walls) {
+          final x1 = wall.start.x;
+          final y1 = wall.start.y;
+          final x2 = wall.end.x;
+          final y2 = wall.end.y;
 
-        final t = ((xp - x1) * (y2 - y1) - (yp - y1) * (x2 - x1)) /
-            (dy * (x2 - x1) - dx * (y2 - y1));
-        final u = ((xp - x1) * dy - (yp - y1) * dx) /
-            ((x2 - x1) * dy - (y2 - y1) * dx);
+          final t = ((xp - x1) * (y2 - y1) - (yp - y1) * (x2 - x1)) /
+              (dy * (x2 - x1) - dx * (y2 - y1));
+          final u = ((xp - x1) * dy - (yp - y1) * dx) /
+              ((x2 - x1) * dy - (y2 - y1) * dx);
 
-        if (u >= 0 && u <= 1 && t >= 0) {
-          final x = x1 + u * (x2 - x1);
-          final y = y1 + u * (y2 - y1);
+          if (u >= 0 && u <= 1 && t >= 0) {
+            final x = x1 + u * (x2 - x1);
+            final y = y1 + u * (y2 - y1);
 
-          if (t < dmin) {
-            dmin = t;
-            xmin = x;
-            ymin = y;
+            if (t < dmin) {
+              dmin = t;
+              xmin = x;
+              ymin = y;
+            }
           }
         }
-      }
 
-      if (dmin < double.infinity) {
-        final v = Offset(xmin, ymin) - position.toOffset();
-        rays.add(v);
-      }
+        if (dmin < kViewMax) {
+          final v = Offset(xmin, ymin) - position.toOffset();
+          rays.add(v);
+        } else {
+          rays.add(
+            (Vector2(xp, yp) + ray * kViewMax).toOffset() - position.toOffset(),
+          );
+        }
 
-      ray.rotate(step);
+        ray.rotate(step);
+      }
     }
 
     if (rays.isNotEmpty) {
